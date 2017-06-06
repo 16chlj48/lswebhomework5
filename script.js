3c61bffa724a371fbfd04b65bf79423d33339299
Chloe Janko <chloenoelle98@gmail.com>
2017-06-06 18:44:25 -0400

    syntax edit

--- script.js
+++ script.js
@@ -58,17 +58,6 @@ for (var i = 1; i<=100; i+=15) {
 }
 
 //EXTRA CREDIT: Fizzbuzz
-
-/*
-
-The "Fizz-Buzz test" is an interview question designed to help filter out the 99.5% of programming job candidates who can't seem to program their way out of a wet paper bag. The text of the programming assignment is as follows:
-
-"Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”."
-
-Hint: Use your last three loops and if/then/else statements. You can learn about those here https://www.w3schools.com/js/js_if_else.asp
-
-*/
-
 for (var i = 1; i <= 100; i++) {
 	if (i % 3 === 0) {
 		console.log("Fizz");
@@ -83,3 +72,14 @@ for (var i = 1; i <= 100; i++) {
 		console.log(i);
 	}
 }
+
+/*
+
+The "Fizz-Buzz test" is an interview question designed to help filter out the 99.5% of programming job candidates who can't seem to program their way out of a wet paper bag. The text of the programming assignment is as follows:
+
+"Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”."
+
+Hint: Use your last three loops and if/then/else statements. You can learn about those here https://www.w3schools.com/js/js_if_else.asp
+
+*/
+
